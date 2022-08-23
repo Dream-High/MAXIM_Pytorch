b689e311ea4da5fbcacc121d4d1e605313e093c7
Dream-High <2360513980@qq.com>
2022-08-23 22:14:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from model import MlpBlock
+import torch
 
-model = MlpBlock(1, 24, 1)
+from model import *
+
+my_model = MAXIM(num_supervision_scales=3, features=32)
+input_test = torch.ones((1, 64, 64, 3))
+y = my_model(input_test)
